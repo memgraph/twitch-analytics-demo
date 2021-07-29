8026b75ff99430c63971a3218e3261c3e1952676
Katarina Šupe <supe.katarina@gmail.com>
2021-07-29 14:01:45 +0200

    added about section

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,13 @@ var svg = d3.select("svg");
 const width = svg.attr("width");
 const height = svg.attr("height");
 var xmlhttp = new XMLHttpRequest();
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
 load_data();
 
@@ -305,4 +312,18 @@ function showSection(section) {
   for (var i = 0; i < filtered.length; i++)
     document.getElementById(filtered[i]).style.display = "none";
   document.getElementById(section).style.display = "inline";
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }

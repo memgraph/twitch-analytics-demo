dfd7efb318b23ba67f6417ccc1f2b6eafbb33cf0
Katarina Šupe <supe.katarina@gmail.com>
2021-07-29 20:45:23 +0200

    Add tooltips

--- public/js/index.js
+++ public/js/index.js
@@ -217,7 +217,7 @@ function get_graph() {
         .data(links)
         .join("line")
         .attr("stroke-width", 1);
-
+      var div = d3.select("#tooltip");
       const node = svg
         .append("g")
         .attr("stroke", "#fff")
@@ -229,6 +229,25 @@ function get_graph() {
         .attr("class", "node")
         .attr("fill", function (d) {
           return d.label === "Team" ? "red" : "orange";
+        })
+        .on("mouseover", function (event, d) {
+          div.transition().duration(200).style("opacity", 0.9);
+          div
+            .html(
+              "label: " +
+                d.label +
+                "<br>" +
+                "id: " +
+                d.id +
+                "<br>" +
+                "name: " +
+                d.name
+            )
+            .style("left", event.pageX + 25 + "px")
+            .style("top", event.pageY - 25 + "px");
+        })
+        .on("mouseout", function (event, d) {
+          div.transition().duration(500).style("opacity", 0);
         })
         .call(drag(simulation));
 
@@ -345,32 +364,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
-
-function responsivefy(svg) {
-  // get container + svg aspect ratio
-  var container = d3.select(svg.node().parentNode),
-    width = parseInt(svg.style("width")),
-    height = parseInt(svg.style("height")),
-    aspect = width / height;
-
-  // add viewBox and preserveAspectRatio properties,
-  // and call resize so that svg resizes on inital page load
-  svg
-    .attr("viewBox", "0 0 " + width + " " + height)
-    .attr("perserveAspectRatio", "xMinYMid")
-    .call(resize);
-
-  // to register multiple listeners for same event type,
-  // you need to add namespace, i.e., 'click.foo'
-  // necessary if you call invoke this function for multiple svgs
-  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
-  d3.select(window).on("resize." + container.attr("id"), resize);
-
-  // get width of container and resize svg to fit it
-  function resize() {
-    var targetWidth = parseInt(container.style("width"));
-    svg.attr("width", targetWidth);
-    svg.attr("height", Math.round(targetWidth / aspect));
-  }
 }

9b4230089f657d3f3078f896b1c3b21efde18198
Katarina Šupe <supe.katarina@gmail.com>
2021-08-12 16:39:01 +0200

    Set up react app

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import "semantic-ui-less/semantic.less";
+import React from "react";
+
+function App() {
+  // fetch("/get-top-teams/1")
+  // .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  return <div></div>;
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //         <button class="ui button">Click Here</button>
+  //       </header>
+  //     </div>
+}
+
+export default App;

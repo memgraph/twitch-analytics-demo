2465c9ebb61c4d61b33271ca063b8d24ee152641
katarinasupe <supe.katarina@gmail.com>
2022-01-24 22:14:01 +0100

    Add exists argument

--- backend/models.py
+++ backend/models.py
@@ -4,12 +4,14 @@ from gqlalchemy import Node, Field, Relationship
 
 
 class User(Node):
-    name: str = Field(index=True, unique=True, db=memgraph)
+    name: str = Field(index=True, exists=True, unique=True, db=memgraph)
 
 
 class Stream(User):
-    name: Optional[str] = Field(index=True, unique=True, db=memgraph, label="User")
-    id: str = Field(index=True, unique=True, db=memgraph)
+    name: Optional[str] = Field(
+        index=True, exists=True, unique=True, db=memgraph, label="User"
+    )
+    id: str = Field(index=True, exists=True, unique=True, db=memgraph)
     url: Optional[str] = Field()
     followers: Optional[int] = Field()
     createdAt: Optional[str] = Field()
